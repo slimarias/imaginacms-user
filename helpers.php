fd1c1977bccb441ba0cc90cf2d52904c9d9c4b23
Carlos <carlosdevia@imaginacolombia.com>
2020-11-10 23:43:01 -0500

    Fix: MediaRelation.php Trait

--- helpers.php
+++ helpers.php
@@ -3,7 +3,7 @@
 if (function_exists('current_permission_value') === false) {
     function current_permission_value($model, $permissionTitle, $permissionAction)
     {
-        $value = array_get($model->permissions, "$permissionTitle.$permissionAction");
+        $value = Arr::get($model->permissions, "$permissionTitle.$permissionAction");
         if ($value === true) {
             return 1;
         }
@@ -18,7 +18,7 @@ if (function_exists('current_permission_value') === false) {
 if (function_exists('current_permission_value_for_roles') === false) {
     function current_permission_value_for_roles($model, $permissionTitle, $permissionAction)
     {
-        $value = array_get($model->permissions, "$permissionTitle.$permissionAction");
+        $value = Arr::get($model->permissions, "$permissionTitle.$permissionAction");
         if ($value === true) {
             return 1;
         }
